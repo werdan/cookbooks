a7fc687fb20cf9092ae4610e39d7a85184a1108e
Andriy Samilyak <samilyak@gmail.com>
2014-10-07 18:20:00 +0300

    PHP SOAP module

---
+++ mod_php5_apache2/attributes/customize.rb
@@ -0,0 +1,19 @@
+  normal[:mod_php5_apache2][:packages] = [
+    'php5-xsl',
+    'php5-curl',
+    'php5-xmlrpc',
+    'php5-sqlite',
+    'php5-dev',
+    'php5-gd',
+    'php5-cli',
+    'php5-sasl',
+    'php5-mcrypt',
+    'php5-memcache',
+    'php-pear',
+    'php-xml-parser',
+    'php-mail-mime',
+    'php-db',
+    'php-mdb2',
+    'php-html-common',
+    'php-soap'
+  ]
